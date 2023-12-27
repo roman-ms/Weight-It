3a5da771d5e0e735852cc03e4e8fb5f809c0739b
Roman <49215713+roman-ms@users.noreply.github.com>
2023-12-27 16:58:14 -0600

    Finished CmeraView setup

---
+++ Weight-It/CameraView.swift
@@ -0,0 +1,56 @@
+//
+//  CameraView.swift
+//  Weight-It
+//
+//  Created by Roman Meshkov on 12/27/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct CameraView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIViewController
+    
+    let cameraService: CameraService
+    let didFinishProcessingPhoto: (Result<AVCapturePhoto, Error>) -> ()
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+
+        cameraService.start(delegate: context.coordinator) { err in
+            if let err = err {
+                didFinishProcessingPhoto(.failure(err))
+                return
+            }
+        }
+
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .black
+        viewController.view.layer.addSublayer(cameraService.previewLayer)
+        cameraService.previewLayer.frame = viewController.view.bounds
+        return viewController
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self, didFinishProcessingPhoto: didFinishProcessingPhoto)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context){ }
+    
+    class Coordinator: NSObject, AVCapturePhotoCaptureDelegate {
+        let parent: CameraView
+        private var didFinishProcessingPhoto: (Result<AVCapturePhoto, Error>) -> ()
+        
+        init(_ parent: CameraView, didFinishProcessingPhoto: @escaping (Result<AVCapturePhoto, Error>) -> ()) {
+            self.parent = parent
+            self.didFinishProcessingPhoto = didFinishProcessingPhoto
+        }
+        
+        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+            if let error = error {
+                didFinishProcessingPhoto(.failure(error))
+                return
+            }
+            didFinishProcessingPhoto(.success(photo))
+        }
+    }
+}
