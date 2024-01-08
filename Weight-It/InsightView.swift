9bb2e070eb4b334da606f17e08437ef653158e04
Roman <49215713+roman-ms@users.noreply.github.com>
2024-01-08 12:30:47 -0600

    Added a scrollable Insight view placeholder

--- Weight-It/InsightView.swift
+++ Weight-It/InsightView.swift
@@ -7,12 +7,39 @@
 
 import Foundation
 import SwiftUI
+
+struct InsightItemView: View {
+    let title: String
+
+    var body: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue)
+                .frame(height: 200)
+
+            Text(title)
+                .foregroundColor(.white)
+                .padding(.leading, 10) // Adjust the padding as needed
+        }
+    }
+}
+
 struct InsightView: View {
     var body: some View {
-        VStack {
-            Text("Insights")
-                .bold()
-            Spacer()
+        ScrollView {
+            VStack {
+                Text("Insights")
+                    .bold()
+                VStack(spacing: 10) {
+                    InsightItemView(title: "Calories")
+                    InsightItemView(title: "Nutrition")
+                    InsightItemView(title: "Calories trend")
+                    InsightItemView(title: "Health")
+                    InsightItemView(title: "Food distribution")
+                }
+                .padding()
+                Spacer()
+            }
         }
     }
 }
