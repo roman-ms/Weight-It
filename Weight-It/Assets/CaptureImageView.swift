afb00cfc5052265ac4cd0abc9e0588a84f96bcec
Roman <49215713+roman-ms@users.noreply.github.com>
2024-01-18 14:34:37 -0600

    Added Photo Picker option to the front screen

--- Weight-It/Assets/CaptureImageView.swift
+++ Weight-It/Assets/CaptureImageView.swift
@@ -1,23 +1,51 @@
 import SwiftUI
+import PhotosUI
 
 struct CaptureImageView: View {
     @Binding var capturedImage: UIImage?
     @Binding var isCustomCameraViewPresented: Bool
+    
+    @State private var photosPickerItem: PhotosPickerItem?
 
     var body: some View {
-        Button(action: {
-            isCustomCameraViewPresented.toggle()
-        }, label: {
-            Image(systemName: "camera.fill")
-                .font(.largeTitle)
-                .padding()
-                .background(Color(AppColors.secondaryColor))
-                .foregroundColor(.white)
-                .clipShape(Circle())
-        })
-        .padding(.bottom)
-        .sheet(isPresented: $isCustomCameraViewPresented, content: {
-            CustomCameraVeiw(capturedImage: $capturedImage)
-        })
+        HStack{
+            Button(action: {
+                isCustomCameraViewPresented.toggle()
+            }, label: {
+                Image(systemName: "camera.fill")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color(AppColors.secondaryColor))
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+            })
+            .sheet(isPresented: $isCustomCameraViewPresented, content: {
+                CustomCameraVeiw(capturedImage: $capturedImage)
+            })
+            
+            //Photo Picker button implementation
+            PhotosPicker(selection: $photosPickerItem,
+                         matching: .images) {
+                Image(systemName: "photo")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color(AppColors.secondaryColor))
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+            }
+            //On change of photosPickerItem run this code
+            .onChange(of: photosPickerItem) { _, _ in
+                Task{
+                    if let photosPickerItem,
+                       let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                        if let image = UIImage(data: data) {
+                            capturedImage = image
+                        }
+                    }
+                    //Erase old selection
+                    photosPickerItem = nil
+                }
+            }
+        }
     }
 }
