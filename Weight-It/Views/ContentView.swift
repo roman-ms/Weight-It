401de2ade55b109de818355ba9db4c46f3eb699c
Roman <49215713+roman-ms@users.noreply.github.com>
2024-04-03 17:46:23 -0500

    Changed defgault view of the app

--- Weight-It/Views/ContentView.swift
+++ Weight-It/Views/ContentView.swift
@@ -12,26 +12,30 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State private var selectedTabIndex = 2 // Assuming ProductsView is intended to be the default
 
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTabIndex){
             InsightView()
                 .tabItem {
                     Text("Insights")
                     Image(systemName: "chart.pie")
-                }
+                }.tag(1)
+                
+            
             ProductsView()
                 .tabItem {
                     Text("Home")
                     Image(systemName: "house.fill")
-                  }
+                  }.tag(2) // This tag matches the initial state, making ProductsView the default
+                
+            
             SettingsView()
                 .tabItem {
                     Text("Settings")
                     Image(systemName: "gearshape")
-                }
+                }.tag(3)
         }
-        .accentColor(Color(AppColors.secondaryColor))
     }
 }
 
