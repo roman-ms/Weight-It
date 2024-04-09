2df72f76631bfeb71b63ac1abd1fc0cad9346ac3
Roman <49215713+roman-ms@users.noreply.github.com>
2024-04-09 14:55:05 -0500

    Modifications in donut chart

--- Weight-It/Elements/SectorChart.swift
+++ Weight-It/Elements/SectorChart.swift
@@ -17,47 +17,100 @@ struct Product: Identifiable {
 struct SectorChart: View {
     let products: [Product] // Use a constant property for the passed products array
     
+    // Define a static array of Products with placeholder values
+    let staticProducts: [Product] = [
+        Product(title: "Protein", revenue: 100),
+        Product(title: "Carbs", revenue: 200),
+        Product(title: "Fat", revenue: 300),
+        Product(title: "Fiber", revenue: 400),
+        Product(title: "Wate", revenue: 500)
+    ]
+    
     // A predefined set of colors for the chart elements
     private let chartColors: [Color] = [Color(hex: "00000A"), Color(hex: "F63A24"), Color(hex: "0D8D60"), Color(hex: "FFCF00"), Color(hex: "0562AE"), Color(hex: "046A54")]
     
     // No need for a default products array inside the struct
     
     var body: some View {
-        HStack {
-            // Chart
-            Chart {
-                ForEach(Array(products.enumerated()), id: \.element.id) { index, product in
-                    SectorMark(
-                        angle: .value(product.title, product.revenue),
-                        innerRadius: .ratio(0.6),
-                        angularInset: 8
-                    )
-                    .foregroundStyle(chartColors[index % chartColors.count]) // Apply color based on index
-                }
-            }
-            
-            // Bullet points with items
-            VStack(alignment: .leading, spacing: 10) {
-                ForEach(Array(products.enumerated()), id: \.element.id) { index, product in
-                    HStack {
-                        Circle()
-                            .fill(chartColors[index % chartColors.count]) // Cycle through the colors
-                            .frame(width: 10, height: 10)
-                        Text(product.title)
+        if products.isEmpty {
+            HStack {
+                // Chart
+                Chart {
+                    ForEach(Array(staticProducts.enumerated()), id: \.element.id) { index, product in
+                        SectorMark(
+                            angle: .value(product.title, product.revenue),
+                            innerRadius: .ratio(0.6),
+                            angularInset: 8
+                        )
+                        .foregroundStyle(chartColors[index % chartColors.count]) // Apply color based on index
                     }
                 }
+                .frame(width: 150)
+                Spacer()
+                    .frame(width:70)
+                // Bullet points with items
+                VStack(alignment: .leading, spacing: 10) {
+                    ForEach(Array(staticProducts.enumerated()), id: \.element.id) { index, product in
+                        HStack {
+                            Circle()
+                                .fill(chartColors[index % chartColors.count]) // Cycle through the colors
+                                .frame(width: 10, height: 10)
+                            Text(product.title)
+                        }
+                    }
+                }
+                .padding(.trailing, 20)
             }
-            .padding(.trailing, 20)
+            .frame(height: 150)
+            .blur(radius: 10)
+            .overlay(
+                Text("No Data")
+                    .font(.title), // Adjust font as needed
+                alignment: .center // Overlay the text in the center of the blurred content
+            )
+        } else {
+            HStack {
+                // Chart
+                Chart {
+                    ForEach(Array(products.enumerated()), id: \.element.id) { index, product in
+                        SectorMark(
+                            angle: .value(product.title, product.revenue),
+                            innerRadius: .ratio(0.6),
+                            angularInset: 8
+                        )
+                        .foregroundStyle(chartColors[index % chartColors.count]) // Apply color based on index
+                    }
+                }
+                .frame(width: 150)
+                Spacer()
+                    .frame(width:70)
+                
+                // Bullet points with items
+                VStack(alignment: .leading, spacing: 10) {
+                    ForEach(Array(products.enumerated()), id: \.element.id) { index, product in
+                        HStack {
+                            Circle()
+                                .fill(chartColors[index % chartColors.count]) // Cycle through the colors
+                                .frame(width: 10, height: 10)
+                            Text(product.title)
+                        }
+                    }
+                }
+                .padding(.trailing, 20)
+            }
+            .frame(height: 150)
         }
     }
-    
 }
 
 
 struct SectorChartExample_Previews: PreviewProvider {
     static var previews: some View {
         SectorChart(products: [
-            Product(title: "Custom Product 1", revenue: 0.5),
-            Product(title: "Custom Product 2", revenue: 0.3)])
+            Product(title: "Protein", revenue: 60),
+            Product(title: "Carbs", revenue: 20),
+            Product(title: "Fat", revenue: 40),
+            Product(title: "Fiber", revenue: 40),
+        ])
     }
 }
